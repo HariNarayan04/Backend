a68ab7f7f68d6a4fb24ef51ab62ba6c04a2479d2
HariNarayan04 <harinarayan06062004@gmail.com>
2025-08-21 00:32:20 +0530

    Updated Location File

--- utils/location.js
+++ utils/location.js
@@ -3,22 +3,30 @@ const HttpError = require('../models/http-error');
 
 const API_KEY = process.env.GOOGLE_API_KEY;
 
-async function getCoordsForAddress(address){
-    const encodedAddress = encodeURIComponent(address);
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${API_KEY}`);
+async function getCoordsForAddress(address) {
+  const encodedAddress = encodeURIComponent(address);
 
-    const data = response.data;
+  let response;
+  try {
+    response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${API_KEY}`
+    );
+  } catch (err) {
+    throw new HttpError('Geocoding request failed, check your network or API key.', 500);
+  }
 
-    if(!data || data.status === 'ZERO_RESULTS'){
-        const error = new HttpError('Could not find location for the specified address', 422);
-        throw error;
-    }
+  const data = response.data;
 
-    const coordinates = data.results[0].geometry.location;
-    
+  if (!data || data.status === 'ZERO_RESULTS') {
+    throw new HttpError('Could not find location for the specified address.', 422);
+  }
 
-    return coordinates;
+  if (data.status !== 'OK') {
+    throw new HttpError(`Geocoding failed with status: ${data.status}`, 500);
+  }
 
+  const coordinates = data.results[0].geometry.location;
+  return coordinates; // { lat, lng }
 }
 
 module.exports = getCoordsForAddress;
